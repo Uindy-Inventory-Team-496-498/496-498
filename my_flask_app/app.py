9f0ee0998296713677b8963083114c4b262df9da
MatthewBilo <91922584+MatthewBilo@users.noreply.github.com>
2024-09-03 13:47:55 -0400

    working

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,10 +1,21 @@
+'''
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker World!'
+    return 'Hello, Docker World!!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+'''
+
+from flask import Flask
+def create_app():
+    app = Flask(__name__)
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+    return app
+app = create_app()
