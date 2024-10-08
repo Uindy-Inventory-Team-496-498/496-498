42f6f64972016ac17cffde9ffc993deaf047d1d7
Nicholas Dwenger <146957026+dwengern@users.noreply.github.com>
2024-10-08 15:41:10 +0000

    Cleaned up code

--- my_flask_app/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-def create_app():
-    app = Flask(__name__)
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!!!'
-    return app
-app = create_app()
