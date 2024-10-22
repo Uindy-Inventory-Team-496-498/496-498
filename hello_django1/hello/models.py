d6b32534166aeefc20e183292736da320870f4bb
leveridgee <90484566+leveridgee@users.noreply.github.com>
2024-10-22 13:40:12 -0400

    Fix pr15 - Issue 2.3 (#17)
    * Put the table structures into Models.py
    
    Only put one Chemical member in each table but it'll be really easy to change to add or remove fields as we see fit.
    
    * remove .pyc
    
    * Revert "Merge branch 'main' into fix-pr15"
    
    This reverts commit cac02908285658cfbc8b4717998d7fcdb2a4baa6, reversing
    changes made to bf25ba78703e99815e9bee9ae368dcc11aa17ef7.
    
    * Conflict Fixes
    
    * Reapply "Merge branch 'main' into fix-pr15"
    
    This reverts commit 84bb089a9467160b50b632f3538886f373515f79.
    
    ---------
    
    Co-authored-by: WilifordA <wiliforda@uindy.edu>

--- hello_django1/hello/models.py
+++ hello_django1/hello/models.py
@@ -4,9 +4,21 @@ from django.utils import timezone
 class LogChemical(models.Model):
     chemical = models.CharField(max_length=300)
     log_date = models.DateTimeField("date logged")
-
     def __str__(self):
         """Returns a string representation of a message"""
         date = timezone.UTC(self.log_date)
         return f"'{self.chemical}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
-    
+	
+class currentlyInStorageTable(models.Model):
+	chemBottleIDNUM = models.IntegerField()
+	chemName = models.CharField(max_length=255)
+	chemLocation = models.CharField(max_length=255)
+	chemAmountInBottle = models.FloatField()
+	chemStorageType = models.CharField(max_length=255)
+	 
+	 
+	 
+class allChemicalsTable(models.Model):
+	chemName = models.CharField(max_length=255)
+	chemLocation = models.CharField(max_length=255)
+	chemStorageType = models.CharField(max_length=255)
