42398235bb078d112ac65e251f4a66d0fb1a1640
leveridgee <leveridgee@uindy.edu>
2024-10-29 00:39:57 -0400

    DB update with fixture file

--- hello_django1/hello/models.py
+++ hello_django1/hello/models.py
@@ -22,3 +22,11 @@ class allChemicalsTable(models.Model):
 	chemName = models.CharField(max_length=255)
 	chemLocation = models.CharField(max_length=255)
 	chemStorageType = models.CharField(max_length=255)
+      
+class QRCodeData(models.Model):
+    qr_code = models.CharField(max_length=255, unique=True)  # Field to store the QR code data
+    name = models.CharField(max_length=255)  # Example additional field
+    description = models.TextField(blank=True, null=True)  # Example additional field
+
+    def __str__(self):
+        return self.name
