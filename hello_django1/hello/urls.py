eb4765f117eb299b7b52b2cec470b01339cdd0e6
leveridgee <90484566+leveridgee@users.noreply.github.com>
2024-10-29 12:31:59 -0400

    Current Chemicals DB view (#22)

--- hello_django1/hello/urls.py
+++ hello_django1/hello/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from hello import views
 from .views import login_view
-from hello.models import LogChemical
+from hello.models import LogChemical, currentlyInStorageTable
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -10,6 +10,12 @@ home_list_view = views.HomeListView.as_view(
     queryset=LogChemical.objects.order_by("-log_date")[:5],
     context_object_name ="chemical_list",
     template_name = "home.html",
+)
+
+curr_list_view = views.ChemListView.as_view(
+    queryset=currentlyInStorageTable.objects.order_by("-chemBottleIDNUM")[:5],
+    context_object_name ="chemical_list_db",
+    template_name = "currchemicals.html",
 )
 
 urlpatterns = [
@@ -21,6 +27,7 @@ urlpatterns = [
     path('scanner/', views.qr_code_scanner, name='scanner'),
     path('scan/', views.qr_code_scan, name='scan'),
     path('search/', views.search_by_qr_code, name='search_by_qr_code'),
+    path('currchemicals/', curr_list_view, name='current_chemicals'),
     path("admin/", admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
