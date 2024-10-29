cc4ebc839dc46f7354d5e39d3ec7262fafdea130
leveridgee <leveridgee@uindy.edu>
2024-10-29 00:40:30 -0400

    Scanner rework - loud

--- hello_django1/hello/urls.py
+++ hello_django1/hello/urls.py
@@ -19,6 +19,8 @@ urlpatterns = [
     path("log/", views.log_chemical, name="log"),
     path("delete/<int:id>/", views.delete_chemical, name='delete_message'),
     path('scanner/', views.qr_code_scanner, name='scanner'),
+    path('scan/', views.qr_code_scan, name='scan'),
+    path('search/', views.search_by_qr_code, name='search_by_qr_code'),
     path("admin/", admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
