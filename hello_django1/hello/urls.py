e014d80e9f4203edb89afe6096ed0fc2ad6041f6
leveridgee <90484566+leveridgee@users.noreply.github.com>
2024-11-05 13:34:52 -0500

    Adam changes fix (#29)

--- hello_django1/hello/urls.py
+++ hello_django1/hello/urls.py
@@ -19,7 +19,6 @@ curr_list_view = views.ChemListView.as_view(
 )
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path("", home_list_view, name="home"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
@@ -30,4 +29,6 @@ urlpatterns = [
     path('search/', views.search_by_qr_code, name='search_by_qr_code'),
     path('currchemicals/', curr_list_view, name='current_chemicals'),
     path("admin/", admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+	path('edit/<int:id>/', views.edit_chemical, name='edit_chemical'), 
 ]
