16bbb8487a5f9aa0b6e1f3a3dff1f05f6158cb2d
leveridgee <leveridgee@uindy.edu>
2024-11-05 14:48:40 -0500

    Clean up a bit

--- hello_django1/hello/forms.py
+++ hello_django1/hello/forms.py
@@ -1,17 +1,11 @@
 from django import forms
-from hello.models import LogChemical
 from django.contrib.auth.forms import AuthenticationForm
 from hello.models import currentlyInStorageTable
-
-
-class LogChemicalForm(forms.ModelForm):
-    class Meta:
-        model = LogChemical
-        fields = ("chemical",) #Trailing comma is required
 
 class CustomLoginForm(AuthenticationForm):
     username = forms.CharField(label='Username', max_length=150)
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
 class EditChemicalForm(forms.ModelForm):
     class Meta:
         model = currentlyInStorageTable
