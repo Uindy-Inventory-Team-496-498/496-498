8a0b310f126d610684c9b60a48758d028584b04e
MatthewBilo <91922584+MatthewBilo@users.noreply.github.com>
2024-11-05 12:30:51 -0500

    changed logout redirect

--- hello_django1/web_project/settings.py
+++ hello_django1/web_project/settings.py
@@ -129,8 +129,6 @@ STATIC_ROOT = BASE_DIR / 'static_collected'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/" 
-LOGOUT_REDIRECT_URL = "/accounts/login/"
-
-LOGIN_REDIRECT_URL = "/" 
+LOGOUT_REDIRECT_URL = "/"
 
 ALLOWED_HOSTS = ['*']
