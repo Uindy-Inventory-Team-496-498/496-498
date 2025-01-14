cd504b69cf1c4e866e032b7c2fb78d817dd35433
leveridgee <90484566+leveridgee@users.noreply.github.com>
2025-01-14 13:22:20 -0500

    Update wait-for-it.sh (#39)

--- hello_django1/wait-for-it.sh
+++ hello_django1/wait-for-it.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Use this script to test if a given TCP host/port are available
-
+#Switched CRLF to LF
 WAITFORIT_cmdname=${0##*/}
 
 echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
