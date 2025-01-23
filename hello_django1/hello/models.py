21b48db22dd71c95e857c8d60ed02547b9e84de6
leveridgee <90484566+leveridgee@users.noreply.github.com>
2025-01-23 13:38:32 -0500

    Load Client data, generalize forms, Live web page, remove automation (#43)
    * Moderate restructuring to account for new data
    
    * Update Readme with more commands
    
    * Alter structure to allow for dynamic forms
    
    This creates a new structure so that the name of the model is passed dynamically, allowing us to reuse the same form in forms.py, as well as the same functions in views.py. The model, as well as important fields for the model that are required to be filled before creation, are stored as a tuple accessed by the models name, which is passed vie the url
    
    * Update manage.py
    
    * Re introduce netcat command for delay
    
    * Data rework
    
    * Create a generalized csv for loading data
    
    This includes all the necessary fields from models.py
    
    * Mount application and reduce automation
    
    I think the automated migrations was causing some issues, so I'm removing them for now. This also mounts the project directory and data that needs to be loaded. Mounting the proj dir allows us to see any changes we make within the app right away.
    
    * Add mysql-client to the django image so we can access sql directly from the Django container
    
    * Create load_csv.py
    
    This script provides a repeatable method for loading the csv data from our client. Can be modified as needed
    
    * Altered models to reflect the data properly
    
    * remove --noinput command
    
    I believe this command was overly-supressing important warnings and errors
    
    * Modify currchemicals.html to match new data
    
    * Rework readme for readability
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Fixup the merge for add chemical changes
    
    * Update README.md
    
    * Update docker-compose.yml
    
    * Update docker-compose.yml

--- hello_django1/hello/models.py
+++ hello_django1/hello/models.py
@@ -1,17 +1,58 @@
 from django.db import models
 from django.utils import timezone
 	
+def get_model_by_name(model_name):
+    model_mapping = {
+        'currentlyinstoragetable': (currentlyInStorageTable, ['chemBottleIDNUM', 'chemName', 'chemLocationCabinet', 'chemAmountInBottle']),
+        'allchemicalstable': (allChemicalsTable, ['id', 'name']),
+    }
+    return model_mapping.get(model_name.lower())
+
 class currentlyInStorageTable(models.Model):
-	chemBottleIDNUM = models.IntegerField()
+	chemBottleIDNUM = models.IntegerField(primary_key=True)
+	chemMaterial = models.CharField(max_length=255)  # Add this line back
 	chemName = models.CharField(max_length=255)
-	chemLocation = models.CharField(max_length=255)
-	chemAmountInBottle = models.FloatField()
-	chemStorageType = models.CharField(max_length=255)
-	 
+	chemLocationRoom = models.CharField(max_length=255, default="None")
+	chemLocationCabinet = models.CharField(max_length=255, default="None")
+	chemLocationShelf = models.CharField(max_length=255, default="None")
+	chemAmountInBottle = models.CharField(max_length=255, default="0")
+	chemAmountUnit = models.CharField(null = True, max_length=255)
+	chemConcentration = models.CharField(null = True, max_length=255)
+	chemSDS = models.CharField(null = True, max_length=1)
+	chemStorageType = models.CharField(null = True, max_length=255)
+	chemNotes = models.CharField(null = True, max_length=255)
+	chemInstrument = models.CharField(null = True, max_length=255)
+	chemTest = models.CharField(null = True, max_length=255)
+	# def save(self, *args, **kwargs):
+	# 	if self.chemBottleIDNUM is None:  # Check if chemBottleIDNUM is not set
+	# 		max_id = currentlyInStorageTable.objects.aggregate(models.Max('chemBottleIDNUM'))['chemBottleIDNUM__max']
+	# 		self.chemBottleIDNUM = (max_id or 0) + 1
+	# 	super(currentlyInStorageTable, self).save(*args, **kwargs)
+
 class allChemicalsTable(models.Model):
-	chemName = models.CharField(max_length=255)
-	chemLocation = models.CharField(max_length=255)
-	chemStorageType = models.CharField(max_length=255)
+	MATERIAL_TYPE_CHOICES = [
+        ('Acid', 'Acid'),
+        ('Flammables', 'Flammables'),
+        ('Non-Flammable Organics', 'Non-Flammable Organics'),
+        ('Inorganics', 'Inorganics'),
+        ('Household Chemicals', 'Household Chemicals'),
+        ('Indicators', 'Indicators'),
+        ('Biochem', 'Biochem'),
+        ('Synthetics', 'Synthetics'),
+        ('Instrumental Chemical', 'Instrumental Chemical'),
+    ]
+	id = models.IntegerField(primary_key=True)
+	material_type = models.CharField(max_length=50, choices=MATERIAL_TYPE_CHOICES, null=True)
+	name = models.CharField(max_length=255, null=True)
+	concentration = models.CharField(max_length=50, blank=True, null=True)
+	amount = models.CharField(max_length=50, blank=True, null=True)
+	location = models.CharField(max_length=255, blank=True, null=True)
+	sds = models.IntegerField(blank=True, null=True)
+	notes = models.TextField(blank=True, null=True)
+	instrument = models.CharField(max_length=255, blank=True, null=True)
+
+	def __str__(self):
+		return self.name
       
 class QRCodeData(models.Model):
     qr_code = models.CharField(max_length=255, unique=True)  # Field to store the QR code data
