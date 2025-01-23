073ea9ee5f0d428f69d455f873a779f43823d8ad
MatthewBilo <91922584+MatthewBilo@users.noreply.github.com>
2025-01-23 13:51:52 -0500

    basic page made

--- hello_django1/hello/urls.py
+++ hello_django1/hello/urls.py
@@ -18,12 +18,11 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-
     path("", views.home, name="home"),
     path('scan/', views.qr_code_scan, name='scan'),
     path("search/", views.search_page, name="search"),
     path('search_by_qr/', views.search_qr_code, name='search_qr_code'),
-
+    path('checkinandout/', views.checkinandout, name='checkinandout'),
     path('currchemicals/', curr_list_view, name='currchemicals'),
     path('current_chemicals/', views.list_chemicals, {'model_name': 'currentlyinstoragetable'}, name='current_chemicals'),
     #path('all_chemicals/', views.list_chemicals, {'model_name': 'allchemicalstable'}, name='all_chemicals'),
@@ -32,5 +31,6 @@ urlpatterns = [
     path('delete_chemical/<str:model_name>/<int:pk>/', views.delete_chemical, name='delete_chemical'),
     path('scanner_add/', views.scanner_add, name='scanner_add'),
     path('add/<str:model_name>/', views.add_chemical, name='add_chemical'),
+    
 
 ]
