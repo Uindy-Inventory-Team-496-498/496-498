9390d1427c2037d7dfac26923a53cb3fde25723b
leveridgee <90484566+leveridgee@users.noreply.github.com>
2025-02-11 13:33:05 -0500

    Clean up, Clean up, Everybody Everywhere (#53)
    * Everybody Everywhere
    
    * Create 0002_remove_currentlyinstoragetable_chemstoragetype_and_more.py

--- chemistry/chemistry_system/models.py
+++ chemistry/chemistry_system/models.py
@@ -11,42 +11,24 @@ def get_model_by_name(model_name):
 
 class currentlyInStorageTable(models.Model):
 	chemBottleIDNUM = models.IntegerField(primary_key=True)
-	chemMaterial = models.CharField(max_length=255)  # Add this line back
+	chemMaterial = models.CharField(max_length=255) 
 	chemName = models.CharField(max_length=255)
 	chemLocationRoom = models.CharField(max_length=255, default="None")
 	chemLocationCabinet = models.CharField(max_length=255, default="None")
 	chemLocationShelf = models.CharField(max_length=255, default="None")
 	chemAmountInBottle = models.CharField(max_length=255, default="0")
-	chemCheckedOut = models.BooleanField(default=False)
 	chemAmountUnit = models.CharField(null = True, max_length=255)
 	chemConcentration = models.CharField(null = True, max_length=255)
 	chemSDS = models.CharField(null = True, max_length=20)
-	chemStorageType = models.CharField(null = True, max_length=255)
 	chemNotes = models.CharField(null = True, max_length=255)
 	chemInstrument = models.CharField(null = True, max_length=255)
+	chemCheckedOut = models.BooleanField(default=False)
 	chemCheckedOutBy = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 	chemCheckedOutDate = models.DateTimeField(null=True, blank=True)
-	# chemAddDate = models.DateTimeField(default=timezone.now)
-	# def save(self, *args, **kwargs):
-	# 	if self.chemBottleIDNUM is None:  # Check if chemBottleIDNUM is not set
-	# 		max_id = currentlyInStorageTable.objects.aggregate(models.Max('chemBottleIDNUM'))['chemBottleIDNUM__max']
-	# 		self.chemBottleIDNUM = (max_id or 0) + 1
-	# 	super(currentlyInStorageTable, self).save(*args, **kwargs)
 
 class allChemicalsTable(models.Model):
-	MATERIAL_TYPE_CHOICES = [
-		('Acid', 'Acid'),
-		('Flammables', 'Flammables'),
-		('Non-Flammable Organics', 'Non-Flammable Organics'),
-		('Inorganics', 'Inorganics'),
-		('Household Chemicals', 'Household Chemicals'),
-		('Indicators', 'Indicators'),
-		('Biochem', 'Biochem'),
-		('Synthetics', 'Synthetics'),
-		('Instrumental Chemical', 'Instrumental Chemical'),
-	]
 	id = models.IntegerField(primary_key=True)
-	material_type = models.CharField(max_length=50, choices=MATERIAL_TYPE_CHOICES, null=True)
+	material_type = models.CharField(max_length=50, null=True)
 	name = models.CharField(max_length=255, null=True)
 	concentration = models.CharField(max_length=50, blank=True, null=True)
 	amount = models.CharField(max_length=50, blank=True, null=True)
