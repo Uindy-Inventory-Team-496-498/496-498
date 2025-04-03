72f4f66f620d44f267bcfe71eda8a9dfc1d26712
leveridgee <leveridgee@uindy.edu>
2025-04-03 13:56:23 -0400

    Update dependencies

--- chemistry/web_project/settings.py
+++ chemistry/web_project/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django_filters',
     'django_cotton',
     'tailwind',
-    'django_browser_reload'
+    'django_browser_reload',
+    "django_htmx"
 ]
 
 TAILWIND_APP_NAME = 'chemistry_system'
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
