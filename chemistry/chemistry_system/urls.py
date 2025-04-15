4bf0ec1513175a7df5453256b0717bf020645ebd
Edward Leveridge <90484566+leveridgee@users.noreply.github.com>
2025-04-15 14:34:48 -0400

    Leveridge option amount (#135)
    * HELL YAH
    
    * Update .gitignore
    
    * Cotton example working
    
    * Add Buttons and fixing
    
    * Generaled by model name, fixed referrer code for import
    
    * some bug fixes
    
    * fixed font error
    
    * Init tailwinds, move some files
    
    * Unsure about this
    
    * Fix instell layer, need to be in workdir first
    
    * Cleanup
    
    * Fix Layout
    
    * bruh
    
    * Couple fixes
    
    * Experimentation
    
    * adjustments
    
    * goop
    
    * testing
    
    * testing
    
    * Update Dockerfile
    
    * Delete site.js
    
    * Update dependencies
    
    * Update layout.html
    
    * some changes
    
    * deleted some stuff
    
    * experimenting
    
    * It works! Is it worth it?
    
    * Experimentation gone wrong
    
    * throwback thursday
    
    * wtf bro
    
    * Fix redirects, add, edit, delete
    
    * Fix imports

--- chemistry/chemistry_system/urls.py
+++ chemistry/chemistry_system/urls.py
@@ -24,26 +24,26 @@ urlpatterns = [
     path("search/", views.search_page, name="search"),
     path('live-search-api/', views.live_search_api, name='live_search_api'),
     path('checkinandout/', views.checkinandout, name='checkinandout'),
-    path('currchemicals/', views.currchemicals, name='currchemicals'),
-    path('allchemicals/', views.allchemicals, name='allchemicals'),
     
     path('current_chemicals/', views.list_chemicals, {'model_name': 'individualChemicals'}, name='current_chemicals'),
     path('add_chemical/<str:model_name>/', views.add_chemical, name='add_chemical'),
     path('edit_chemical/<str:model_name>/<int:pk>/', views.edit_chemical, name='edit_chemical'),
     path('delete_chemical/<str:model_name>/<int:pk>/', views.delete_chemical, name='delete_chemical'),
-    path('scanner_add/', views.scanner_add, name='scanner_add'),
+    path('scanner_add/<str:model_name>/', views.scanner_add, name='scanner_add'),
     path('add/<str:model_name>/', views.add_chemical, name='add_chemical'),
     path('export_chemicals_csv/', export_chemicals_csv, name='export_chemicals_csv'),  
     path('import_chemicals_csv/', import_chemicals_csv, name='import_chemicals_csv'),  
-    path('update-checkout-status/<str:model_name>/<str:qrcode_value>/', update_checkout_status, name='update_checkout_status'),  # Update the URL pattern
+    path('update-checkout-status/<str:model_name>/<str:qrcode_value>/', update_checkout_status, name='update_checkout_status'),  
     path('delete_all_chemicals/', views.delete_all_chemicals, name='delete_all_chemicals'),
     path('print/', views.print_page, name='print_page'),
     path('download-qr-pdf/', views.generate_qr_pdf_view, name='download_qr_pdf'), 
     path('log/', views.log, name='log'),
-    path('run-populate-storage/', views.run_populate_storage, name='run_populate_storage'),  # Add the URL pattern
+    path('run-populate-storage/', views.run_populate_storage, name='run_populate_storage'),  
     path('chemical-autocomplete/', ChemicalAutocomplete.as_view(), name='chemical-autocomplete'),
 
-    path('show_all_chemicals/', views.show_all_chemicals, name='show_all_chemicals'),
     path("chem_display/<str:table_name>/", views.chem_display, name="chem_display"),
     path('force-update-total-amount/', views.force_update_total_amount, name='force_update_total_amount'),
+
+    path("__reload__/", include("django_browser_reload.urls")),  
+
 ]
