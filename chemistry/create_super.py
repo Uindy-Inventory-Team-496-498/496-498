b1e9f75e3ef99eb8bcf9233a1d149aec7d548bc9
WilifordA <wiliforda@uindy.edu>
2025-04-24 16:24:44 -0400

    Added groups and un automated creating superuser
    Added groups and un automated creating superuser, they'll just need to run the python file on first startup to create the super user and permission groups

--- chemistry/create_super.py
+++ chemistry/create_super.py
@@ -5,6 +5,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_project.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
 
 User = get_user_model()
 
@@ -12,6 +14,17 @@ User = get_user_model()
 SUPERUSER_USERNAME = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
 SUPERUSER_EMAIL = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@uindy.edu')
 SUPERUSER_PASSWORD = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminpass')
+
+# Define required groups
+REQUIRED_GROUPS = ['Admin', 'Professors', 'Students']
+
+# Create required groups if they don't exist
+for group_name in REQUIRED_GROUPS:
+    group, created = Group.objects.get_or_create(name=group_name)
+    if created:
+        print(f"Group '{group_name}' created.")
+    else:
+        print(f"Group '{group_name}'already exists.")
 
 # Create the superuser if it doesn't exist
 if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
