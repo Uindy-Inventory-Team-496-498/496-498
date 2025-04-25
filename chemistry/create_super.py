2d21485f02550ae4d80f412b17df3c0e9c0c2d32
Edward Leveridge <90484566+leveridgee@users.noreply.github.com>
2025-04-25 18:52:01 +0000

    make sure admin in admin

--- chemistry/create_super.py
+++ chemistry/create_super.py
@@ -7,7 +7,6 @@ django.setup()
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 
-
 User = get_user_model()
 
 # Define superuser credentials
@@ -24,15 +23,24 @@ for group_name in REQUIRED_GROUPS:
     if created:
         print(f"Group '{group_name}' created.")
     else:
-        print(f"Group '{group_name}'already exists.")
+        print(f"Group '{group_name}' already exists.")
 
 # Create the superuser if it doesn't exist
 if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
-    User.objects.create_superuser(
+    superuser = User.objects.create_superuser(
         username=SUPERUSER_USERNAME,
         email=SUPERUSER_EMAIL,
         password=SUPERUSER_PASSWORD,
     )
     print(f"Superuser {SUPERUSER_USERNAME} created successfully.")
 else:
+    superuser = User.objects.get(username=SUPERUSER_USERNAME)
     print(f"Superuser {SUPERUSER_USERNAME} already exists.")
+
+# Add the superuser to the Admin group
+admin_group = Group.objects.get(name='Admin')
+if not superuser.groups.filter(name='Admin').exists():
+    superuser.groups.add(admin_group)
+    print(f"Superuser {SUPERUSER_USERNAME} added to the 'Admin' group.")
+else:
+    print(f"Superuser {SUPERUSER_USERNAME} is already in the 'Admin' group.")
